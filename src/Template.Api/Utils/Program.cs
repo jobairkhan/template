2f5739f331e039f5ac0c4d54080fcd2ca30b944f
Jobair Khan <m.j.khan.bd@gmail.com>
2018-04-05 16:19:35 +0100

    Serilog

--- src/Template.Api/Utils/Program.cs
+++ src/Template.Api/Utils/Program.cs
@@ -1,20 +1,50 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Events;
 
 namespace Api.Utils
 {
     public class Program
     {
+        private static readonly string BaseDirectory = AppDomain.CurrentDomain.BaseDirectory;
         public static void Main(string[] args)
         {
+            Environment.SetEnvironmentVariable("BASEDIR", BaseDirectory);
+
             BuildWebHost(args).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args)
-        {
-            return WebHost.CreateDefaultBuilder(args)
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(ConfigConfiguration)
+                .UseSerilog(LoggingConfiguration)
                 .UseStartup<Startup>()
                 .Build();
+
+        private static void LoggingConfiguration(WebHostBuilderContext hostingContext, LoggerConfiguration loggingConfig)
+        {
+            loggingConfig
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .ReadFrom.Configuration(hostingContext.Configuration);
+        }
+
+        private static void ConfigConfiguration(WebHostBuilderContext ctx, IConfigurationBuilder config)
+        {
+            var env = ctx.HostingEnvironment;
+            
+            config.Sources.Clear();
+
+            config
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            config.Build();
         }
     }
 }
